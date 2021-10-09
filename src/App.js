72b1b1cef269e27b884efe5ca49ab797d34f5b0d
Allan Carvalho <allandcarv@gmail.com>
2021-10-09 19:34:31 +0100

    feat: add a meals component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Meals from './components/Meals';
 import Header from './components/Layout/Header';
 
-const App = () => <Header />
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <Meals />
+            </main>
+        </>
+    )
+}
 
 export default App;
